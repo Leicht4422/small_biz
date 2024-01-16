6d7c636075564b66a46d95a634554ef2e43719a8
Leicht4422 <kengflora@LEICHT>
2024-01-16 13:29:54 +0300

    Initial commit.

---
+++ src/small_biz_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { small_biz_backend } from "../../declarations/small_biz_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await small_biz_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
